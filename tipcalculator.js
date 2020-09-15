e3a6024447b2a19195b97cd5337d525668587e5e
Uendi <hoxhauendi21@gmail.com>
2020-09-15 12:51:41 +0200

    initial commit

---
+++ tipcalculator.js
@@ -0,0 +1,36 @@
+function calculateTip() {
+  var billAmt = document.getElementById("billamt").value;
+  var serviceQual = document.getElementById("serviceQual").value;
+  var numOfPeople = document.getElementById("peopleamt").value;
+
+  //valid input
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+  }
+  //Check to see if this input is empty or less than or equal to 1
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //Calculate tip
+  var total = (billAmt * serviceQual) / numOfPeople;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+}
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
